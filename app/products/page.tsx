74ef58e276eb30a01186b7d053b28e01c15ecf8e
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2024-01-14 15:44:09 +0530

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -37,27 +37,9 @@ const Page = () => {
 
           <div className="mt-8 sm:flex sm:items-center sm:justify-between">
             <div className="block sm:hidden">
-              <button className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
-                <span className="text-sm font-medium"> Filters & Sorting </span>
-
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-4 w-4 rtl:rotate-180"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                  />
-                </svg>
-              </button>
             </div>
 
-            <div className="hidden sm:block">
+            <div className="sm:block">
               <label htmlFor="SortBy" className="sr-only">
                 Sort By
               </label>
